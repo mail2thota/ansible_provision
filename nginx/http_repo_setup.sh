bac2ed1c28e60f1e65e5a5bd702c041c90fb202b
Satya.Thota <Satya.Thota@baesystems.com>
2017-08-25 13:13:19 +0800

    http repo setup

---
+++ nginx/http_repo_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum install epel-release
+sudo yum install nginx
+systemctl start nginx
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+sudo systemctl enable nginx
+
