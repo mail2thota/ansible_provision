ccf6e513e5c55f6dbf2ee321eea1db2e8e8bd323
satyathota <satya.thota@baesystems.com>
2017-08-28 12:35:50 +0800

    script update

--- nginx/http_repo_setup.sh
+++ nginx/http_repo_setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 yum install -y epel-release
 yum install -y nginx
+
+#create a static "repo" directory to be listed
 mkdir -p /usr/share/nginx/html/repo/
-#This must be changed to real repo config or downloads
-touch index.html
-cp index.html /usr/share/nginx/html/repo/
+
+#copy all the pre downloaded dependencies from /usr/share/repo/ to nginx  
+cp /usr/share/repo/ /usr/share/nginx/html/repo/
+
+#provide permissions to nginx
 chown :nginx /usr/share/nginx/html/repo/ 
 
+#copy the custom nginx conf to enable repo directory listing & start the nginx service
 systemctl start nginx
+
+#firewall config
 firewall-cmd --permanent --zone=public --add-service=http 
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
-cp nginx.conf /etc/nginx/
+
+#enable nginx
 systemctl enable nginx
 
