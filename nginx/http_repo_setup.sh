dc5bb3ec4155a5a6adfe983661c07810b58d9640
Satya.Thota <Satya.Thota@baesystems.com>
2017-09-08 16:26:46 +0800

    added sudo

--- nginx/http_repo_setup.sh
+++ nginx/http_repo_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum -y install createrepo epel-release firewalld rsync && yum -y install nginx
-yum -y update && systemctl reboot
+sudo yum -y install createrepo epel-release firewalld rsync && sudo yum -y install nginx
+sudo yum -y update && sudo systemctl reboot
 
 
 
@@ -8,56 +8,56 @@ yum -y update && systemctl reboot
 if [[ $1 == N ]]; then
 
 	#create a "repos" directory to be listed
-	mkdir -p /usr/share/nginx/html/repos/centos/7/os/x86_64 	# Base and Update Repos
-	mkdir -p /usr/share/nginx/html/repos/centos/7/updates/x86_64
-	mkdir -p /usr/share/nginx/html/repos/epel/7/x86_64	# EPEL Repo
-	mkdir -p /usr/share/nginx/html/repos/el/7/PC1/x86_64/	#Puppet Labs Repo
-	mkdir -p /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
-	mkdir -p /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman  plugins
+	sudo mkdir -p /usr/share/nginx/html/repos/centos/7/os/x86_64 	# Base and Update Repos
+	sudo mkdir -p /usr/share/nginx/html/repos/centos/7/updates/x86_64
+	sudo mkdir -p /usr/share/nginx/html/repos/epel/7/x86_64	# EPEL Repo
+	sudo mkdir -p /usr/share/nginx/html/repos/el/7/PC1/x86_64/	#Puppet Labs Repo
+	sudo mkdir -p /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
+	sudo mkdir -p /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman  plugins
 
-	createrepo /usr/share/nginx/html/repos/centos/7/os/x86_64/	# Initialize CentOS Base Repo
-	createrepo /usr/share/nginx/html/repos/centos/7/updates/x86_64/	# Initialize CentOS Update Repo
-	createrepo /usr/share/nginx/html/repos/epel/7/x86_64/	# Initialize EPEL 7 Repo
-	createrepo /usr/share/nginx/html/repos/el/7/PC1/x86_64/	# nitialize Puppet Labs Re[p
-	createrepo /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Initialize Foreman 
-	createrepo /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Initialize Foreman  plugins
+	sudo createrepo /usr/share/nginx/html/repos/centos/7/os/x86_64/	# Initialize CentOS Base Repo
+	sudo createrepo /usr/share/nginx/html/repos/centos/7/updates/x86_64/	# Initialize CentOS Update Repo
+	sudo createrepo /usr/share/nginx/html/repos/epel/7/x86_64/	# Initialize EPEL 7 Repo
+	sudo createrepo /usr/share/nginx/html/repos/el/7/PC1/x86_64/	# nitialize Puppet Labs Re[p
+	sudo createrepo /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Initialize Foreman 
+	sudo createrepo /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Initialize Foreman  plugins
 
 	#copy all the pre downloaded dependencies from internet to nginx directory
-	rsync -avz --exclude='repo*' rsync://mirror.cisp.com/CentOS/7/os/x86_64/ /usr/share/nginx/html/repos/centos/7/os/x86_64/   # CentOS Base Repo
-	rsync -avz --exclude='repo*' rsync://mirror.cisp.com/CentOS/7/updates/x86_64/ /usr/share/nginx/html/repos/centos/7/updates/x86_64/   # CentOS Update Repo
-	rsync -avz --exclude='repo*' --exclude='debug' rsync://mirrors.rit.edu/epel/7/x86_64/ /usr/share/nginx/html/repos/epel/7/x86_64/   # EPEL 7 Repo
-	rsync -avz --exclude='repo*' rsync://yum.puppetlabs.com/el/7/PC1/x86_64/ /usr/share/nginx/html/repos/el/7/PC1/x86_64/ # Puppet Labs repo
-	rsync -avz --exclude='repo*' rsync://yum.theforeman.org/releases/1.15/el7/x86_64/ /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
-	rsync -avz --exclude='repo*' rsync://yum.theforeman.org/plugins/1.15/el7/x86_64/ /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
+	sudo rsync -avz --exclude='repo*' rsync://mirror.cisp.com/CentOS/7/os/x86_64/ /usr/share/nginx/html/repos/centos/7/os/x86_64/   # CentOS Base Repo
+	sudo rsync -avz --exclude='repo*' rsync://mirror.cisp.com/CentOS/7/updates/x86_64/ /usr/share/nginx/html/repos/centos/7/updates/x86_64/   # CentOS Update Repo
+	sudo rsync -avz --exclude='repo*' --exclude='debug' rsync://mirrors.rit.edu/epel/7/x86_64/ /usr/share/nginx/html/repos/epel/7/x86_64/   # EPEL 7 Repo
+	sudo rsync -avz --exclude='repo*' rsync://yum.puppetlabs.com/el/7/PC1/x86_64/ /usr/share/nginx/html/repos/el/7/PC1/x86_64/ # Puppet Labs repo
+	sudo rsync -avz --exclude='repo*' rsync://yum.theforeman.org/releases/1.15/el7/x86_64/ /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
+	sudo rsync -avz --exclude='repo*' rsync://yum.theforeman.org/plugins/1.15/el7/x86_64/ /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
 
-	createrepo --update /usr/share/nginx/html/repos/centos/7/os/x86_64/   # CentOS Base Repo
-	createrepo --update /usr/share/nginx/html/repos/centos/7/updates/x86_64/   # CentOS Update Repo
-	createrepo --update /usr/share/nginx/html/repos/epel/7/x86_64/   # EPEL 7 Repo
-	createrepo --update /usr/share/nginx/html/repos/el/7/PC1/x86_64/ # Puppet Labs repo
-	createrepo --update /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
-	createrepo --update /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
+	sudo createrepo --update /usr/share/nginx/html/repos/centos/7/os/x86_64/   # CentOS Base Repo
+	sudo createrepo --update /usr/share/nginx/html/repos/centos/7/updates/x86_64/   # CentOS Update Repo
+	sudo createrepo --update /usr/share/nginx/html/repos/epel/7/x86_64/   # EPEL 7 Repo
+	sudo createrepo --update /usr/share/nginx/html/repos/el/7/PC1/x86_64/ # Puppet Labs repo
+	sudo createrepo --update /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
+	sudo createrepo --update /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
 else 
 	
 	if [ ! -d /usr/share/nginx/html/repos/ ]; then
 		# copy downloaded folders to nginx repos
-		mkdir -p /usr/share/nginx/html/repos/
+		sudo mkdir -p /usr/share/nginx/html/repos/
 	fi
-	cp /usr/share/repos/ /usr/share/nginx/html/repos/
+	sudo cp /usr/share/repos/ /usr/share/nginx/html/repos/
 
 fi
 
 #provide permissions to nginx
-chown :nginx /usr/share/nginx/html/repos/ 
+sudo chown :nginx /usr/share/nginx/html/repos/ 
 
 #copy the custom nginx conf to enable repo directory listing & start the nginx service
-cp nginx.conf /etc/nginx/
-systemctl start nginx
+sudo cp nginx.conf /etc/nginx/
+sudo systemctl start nginx
 
 #firewall config
-firewall-cmd --permanent --zone=public --add-service=http 
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --reload
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
 
 #enable nginx
-systemctl enable nginx
+sudo systemctl enable nginx
 
