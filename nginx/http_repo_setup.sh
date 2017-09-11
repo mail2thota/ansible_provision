ecdac22a29c84efe3d0fcf156ff8102f2fc8c801
Satya.Thota <Satya.Thota@baesystems.com>
2017-09-11 14:56:52 +0800

    update

--- nginx/http_repo_setup.sh
+++ nginx/http_repo_setup.sh
@@ -4,7 +4,8 @@ sudo yum -y install nginx
 sudo yum -y update
 
 # Create Repository
-if [[ $1 == N ]]; then
+if [[ $1 == N ]]
+ then
 	#create a "repos" directory to be listed
 	mkdir -p /usr/share/nginx/html/repos/centos/7/os/x86_64 	# Base and Update Repos
 	mkdir -p /usr/share/nginx/html/repos/centos/7/updates/x86_64
@@ -35,7 +36,8 @@ if [[ $1 == N ]]; then
 	createrepo --update /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
 	createrepo --update /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
 
-elif [ ! -d /usr/share/nginx/html/repos/ ]; then
+elif [ ! -d /usr/share/nginx/html/repos/ ]
+ then
 	# copy downloaded folders to nginx repos
 	mkdir -p /usr/share/nginx/html/repos/
 	cp /usr/share/repos/ /usr/share/nginx/html/repos/
