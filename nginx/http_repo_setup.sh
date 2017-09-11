fa3fbf5a7f3fdcc159b709ed152f1845e518e626
Satya.Thota <Satya.Thota@baesystems.com>
2017-09-11 12:13:44 +0800

    update

--- nginx/http_repo_setup.sh
+++ nginx/http_repo_setup.sh
@@ -5,7 +5,6 @@ sudo yum -y update
 
 # Create Repository
 if [[ $1 == N ]]; then
-
 	#create a "repos" directory to be listed
 	mkdir -p /usr/share/nginx/html/repos/centos/7/os/x86_64 	# Base and Update Repos
 	mkdir -p /usr/share/nginx/html/repos/centos/7/updates/x86_64
@@ -35,11 +34,11 @@ if [[ $1 == N ]]; then
 	createrepo --update /usr/share/nginx/html/repos/el/7/PC1/x86_64/ # Puppet Labs repo
 	createrepo --update /usr/share/nginx/html/repos/releases/1.15/el7/x86_64/ # Foreman Repo
 	createrepo --update /usr/share/nginx/html/repos/plugins/1.15/el7/x86_64/	# Foreman Plugins
-fi
-if [ ! -d /usr/share/nginx/html/repos/ ]; then
-		# copy downloaded folders to nginx repos
-		mkdir -p /usr/share/nginx/html/repos/
-		cp /usr/share/repos/ /usr/share/nginx/html/repos/
+
+elif [ ! -d /usr/share/nginx/html/repos/ ]; then
+	# copy downloaded folders to nginx repos
+	mkdir -p /usr/share/nginx/html/repos/
+	cp /usr/share/repos/ /usr/share/nginx/html/repos/
 fi
 
 #provide permissions to nginx
