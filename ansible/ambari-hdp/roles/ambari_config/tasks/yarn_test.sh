4b84ac6c550b314de5f17c5e2c999b1244bf4ca8
mradhakrishnan <manikandan.radhakrishnan@baesystems.com>
2017-10-11 17:12:39 +0800

    edited yarn_test.sh for test script

--- ansible/ambari-hdp/roles/ambari_config/tasks/yarn_test.sh
+++ ansible/ambari-hdp/roles/ambari_config/tasks/yarn_test.sh
@@ -1,6 +1,6 @@
 yarn jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-examples.jar pi 16 1000 > yarn_test
 
-yarn_output=$(awk '$1=$1' ORS='\\n' /yarn_test)
+yarn_output=$(awk '$1=$1' ORS='\\n' yarn_test)
 
 if [[ $yarn_output == *"Estimated value of Pi is 3.14250000000000000000"* ]];then
 	echo "yarn is working fine as expected"
