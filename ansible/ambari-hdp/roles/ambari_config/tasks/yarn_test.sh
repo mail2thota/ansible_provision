92d4255b7352398c8f15bfb15ff4ff6857656858
mradhakrishnan <manikandan.radhakrishnan@baesystems.com>
2017-10-11 17:22:18 +0800

    change the message for yarn

--- ansible/ambari-hdp/roles/ambari_config/tasks/yarn_test.sh
+++ ansible/ambari-hdp/roles/ambari_config/tasks/yarn_test.sh
@@ -5,7 +5,7 @@ yarn_output=$(awk '$1=$1' ORS='\\n' yarn_test)
 if [[ $yarn_output == *"Estimated value of Pi is 3.14250000000000000000"* ]];then
 	echo "yarn is working fine as expected"
 else
-	echo "hdfs is not working as expected"
+	echo "yarn is not working as expected"
 fi
 
 
