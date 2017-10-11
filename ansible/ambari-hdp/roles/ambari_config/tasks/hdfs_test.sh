3cb0c7934dedee24105dc8f6010208719a3400c8
mradhakrishnan <manikandan.radhakrishnan@baesystems.com>
2017-10-11 16:19:07 +0800

    added test case for hdfs and yarn

---
+++ ansible/ambari-hdp/roles/ambari_config/tasks/hdfs_test.sh
@@ -0,0 +1,32 @@
+echo "test test" > wordcountfile
+
+sudo -u hdfs hdfs dfs -chmod 777 /
+
+/usr/hdp/2.6.2.0-205/hadoop/bin/hdfs dfs -mkdir /wordcount
+
+/usr/hdp/2.6.2.0-205/hadoop/bin/hdfs dfs -mkdir /wordcount/input
+
+/usr/hdp/2.6.2.0-205/hadoop/bin/hdfs dfs -mkdir /wordcount/output
+
+/usr/hdp/2.6.2.0-205/hadoop/bin/hdfs dfs -put wordcountfile /wordcount/input
+
+su hdfs /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop jar /usr/hdp/2.6.2.0-205/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount /wordcount/input/wordcountfile /wordcount/output/wordcountfile-output
+
+sudo atd
+
+su hdfs /usr/hdp/2.6.2.0-205/hadoop/bin/hadoop dfs -cat /wordcount/output/wordcountfile-output/part-r-00000
+
+/usr/hdp/2.6.2.0-205/hadoop/bin/hdfs dfs -getmerge /wordcount/output/wordcountfile-output /tmp/wordcountfile-output
+
+head /tmp/wordcountfile-output 
+
+wordcountfile_output=$(awk '$1=$1' ORS='\\n' /tmp/wordcountfile-output)
+
+if [[ $wordcountfile_output == "test 2" ]];then
+	echo "hdfs is working fine as expected"
+else
+	echo "hdfs is not working as expected"
+fi
+
+
+
