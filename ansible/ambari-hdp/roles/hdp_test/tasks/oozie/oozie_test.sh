902c2a1b2a86c29e9020ec110aab8725b717ad77
Sateesh Telaprolu <STelaprolu@Imp.net>
2017-11-10 07:01:56 +0000

    Configuring yml files using playbook

--- ansible/ambari-hdp/roles/hdp_test/tasks/oozie/oozie_test.sh
+++ ansible/ambari-hdp/roles/hdp_test/tasks/oozie/oozie_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+oozie_url=$1 
+oozie_name_node=$2 
+oozie_job_tracker=$3
+echo "${oozie_url}"
+sed -i "s%<OOZIE_NAME_NODE>%${oozie_name_node}%g" /home/hdfs/oozie/oozie-examples/job.properties
+sed -i "s%<OOZIE_JOB_TRACKER>%${oozie_job_tracker}%g" /home/hdfs/oozie/oozie-examples/job.properties 
+
 hadoop fs -rm -r /workflows/
 hadoop fs -mkdir /workflows/
 hadoop fs -put /home/hdfs/oozie/oozie-examples /workflows/oozie-examples
-export OOZIE_URL=http://agent4-ambariagent.example.com:11000/oozie
+export OOZIE_URL="${oozie_url}"
 oozie job -config /home/hdfs/oozie/oozie-examples/job.properties -run
