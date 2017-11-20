5485fe358a1cabcd2043e74d97b9131d61c1e020
hsutrisno <harry.sutrisno@baesystems.com>
2017-11-20 08:04:39 +0000

    transform foreman's launching by using ansible

--- foreman-ansible/ansible/roles/fmclient/files/foreman_yml/README.rst
+++ foreman-ansible/ansible/roles/fmclient/files/foreman_yml/README.rst
@@ -42,34 +42,14 @@ Prerequisite
             - restart network; "systemctl restart network"
     5. making sure that you don't have any DHCP server available which is been connecting to the subnet network
     6. making sure that your network device interface or network interface e.g; "enp0s3" is dedicated only for single IP(on the future we are going to support single network interface may have multiple IP )
-    7. The best approach is for having static ip for bootstrap machine, check how to setup static ip below:
---------------
-Static IP
--------------
-    #static ip for bootstrap machine:
-    #cat /etc/sysconfig/network-scripts/ifcfg-enp0s3
-
-    BOOTPROTO="none"
-    IPADDR="10.11.12.7"
-    NETMASK="255.255.255.0"
-    GATEWAY="10.11.12.1"
-    DEVICE=enp0s3
-    HWADDR="08:00:27:78:b1:a1"
-    ONBOOT=yes
-    PEERDNS=yes
-    PEERROUTES=yes
-    DEFROUTE=yes
-    DNS1="10.11.12.7"
-
-
 
 Configuration
 -------------
 
 | Variable       |  Example           | Description  |
 |:------------- |:-------------|:-----|
-|auth:[fqdn,ip,user,pass]|[fqdn:foreman.example.com],[ip:10.11.12.7],[user:admin],[pass:bae4533]|[fqdn:fqdn of foreman(resolvable of fqdn,required)],[ip:ip of foreman(valid ip,required)],[user: user of foreman(string, required)],[pass:password of foreman(string, required)]|
-|domain:[name,fullname]|[name:baesystemdemo.com],[fullname:full description]|[name:valid of domain's name(str,required)],[fullname:clear of description(str,optional)]|
+|auth:[fqdn,ip,user,pass]|[fqdn:foreman.example.com],[ip:10.11.12.7],[user:admin],[pass:bae4533]|[fqdn:fqdn of foreman],[ip:ip of foreman],[user: user of foreman],[pass:password of foreman]|
+|domain:[name,fullname]|[name:baesystemdemo.com],[fullname:full description]|[name:valid domain name],[fullname:clear description]|
 |subnet:[name,network,mask,gateway,dns-primary,dns-secondary,vlanid,domain:[name]]|[name:subnet1012],[network:10.11.12.0],[mask:255.255.255.0],[gateway:10.11.12.1],[dns-primary:10.11.12.7],[dns-secondary:8.8.8.8],[vlanid:1],[domain:[name:example.com]]|[name:name of subnet(str,required)],[network:valid of subnet network ip(str,required)],[mask:valid of mask address(str,required)],[gateway:valid of gateway address(str,optional)],[dns-primary:valid of dns ip(str,optional)],[dns-secondary:valid of dns ip(str,optional)],[vlanid:valid of vlanid(int,optional)],[domain:[name:domain name(str,required)]]|
 |hostgroup:[name,subnet,domain,root-pass]|[name:hostg_master],[subnet:subnet1012],[domain:example.com],[root-pass:abs12232]|[name:name of hostgroup(str,required)],[subnet:name of subnet to be assigned(str,required)],[domain:name of domain to be assigned(str,required)],[root-pass:default password of nodes(str,required,minimum 8 char)]|
 |hosts:[name,hostgroup,ip,mac]|[name:agent_node],[hostgroup:hostg_master],[ip:10.11.12.4],[mac:080027d487f5]|[name:host name(str,required)],[hostgroup:assigned host group(str,required)],[ip:valid of host ip(str,required)],[mac:valid mac address(str,required)]|
@@ -176,22 +156,30 @@ Installation and Provisioning Foreman
 -------------------------------------
 
     git clone ssh://git@10.37.0.35:7999/ta/mdr_platform_bare_metal.git
-    * cd /foreman-ansible/ansible
-    * configure:
-          - /foreman-ansible/ansible/templates/payload.yml
-          - /foreman-ansible/ansible/inventory
-          - /foreman-ansible/ansible/bootup.sh
-     * set correct url_repo in /foreman-ansible/ansible/bootup.sh
-
-    * launch:
-          - cd /foreman-ansible/ansible/
-          - ./bootup.sh
+    cd foreman
+    ./boot.sh
 
     noted:when you see provisioning is ready you might turn up the nodes to be provisioned,
     from bios setting you may choose boot from network and allow boot using PXELinux
 
 
+
+Usage
+-----
+    run as standalone provisioning:
+    1. Install using tar:
+        * su root
+        * pip install fmclient-0.0.1.tar.gz
+    2. install from source:
+        * su root
+        * checkout : git clone ssh://git@10.37.0.35:7999/ta/mdr_platform_bare_metal.git
+        * cd foreman_yml
+        * pip install -e .                      
+    2. import YAML file: fmclient import /path/filename.yml
+    3. turn on/restart your nodes for provisioning setup
 ---
+
+    trace log through tail -f /var/log/foreman-installer/foreman.log
 
 Installation Of Ansible
 -----------------------
---
+++ foreman-ansible/ansible/roles/fmclient/files/foreman_yml/src/foreman_base.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#Copyright:@BaeSystemsAI
+#Original author: Heri Sutrisno
+#Email:harry.sutrisno@baesystems.com
+#Description: base class for foreman-python
+
+import sys
+import logging
+import log
+import socket
+import urllib3
+from foreman.client import Foreman
+from validator import Validator
+from voluptuous import MultipleInvalid
+
+
+class ForemanBase:
+
+    def __init__(self, config, loglevel=logging.INFO):
+        logging.basicConfig(level=loglevel)
+        log.LOGLEVEL = loglevel
+        self.config = config['foreman']
+        self.loglevel = loglevel
+        self.validator = Validator()
+
+    def get_config_section(self, section):
+        try:
+            cfg = self.config[section]
+        except:
+            cfg = []
+        return cfg
+
+    def get_protocol(self):
+        setting_sect = self.get_config_section('setting')[0]
+        try:
+            self.validator.setting(setting_sect)
+        except MultipleInvalid as e:
+            log.log(log.LOG_ERROR, "Cannot set protocol '{0}': \n YAML validation Error: {1}".format(setting_sect, e))
+            sys.exit(1)
+            
+        protocol = setting_sect['protocol']
+        return protocol
+        
+
+    def get_fm_ip(self):
+        hostname = socket.gethostname()
+        ip_hostname = socket.gethostbyname(hostname)
+        foreman_ip = "{0}://".format(self.get_protocol())
+        foreman_ip += ip_hostname
+        return foreman_ip
+
+    def get_fm_hostname(self):
+        hostname = socket.gethostname()
+        return hostname
+
+    def connect(self):
+        log.log(log.LOG_INFO, "Establish connection to Foreman server")
+        foreman_ip = self.get_fm_ip()
+        try:
+            logging.disable(logging.WARNING)
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+            self.fm = Foreman(foreman_ip, (self.config['auth']['user'], self.config['auth']['pass']), api_version=2, use_cache=False, strict_cache=False)
+            # this is nescesary for detecting faulty credentials in yaml
+            self.fm.architectures.index()
+            logging.disable(self.loglevel-1)
+        except:
+            log.log(log.LOG_ERROR, "Cannot connect to Foreman-API")
+            sys.exit(1)
+
+    def get_api_error_msg(self, e):
+        dr = e.res.json()
+        try:
+            msg = dr['error']['message']
+        except KeyError:
+            msg = dr['error']['full_messages'][0]
+
+        return msg
+
+    def update(ipaddress, hostname):
+        """
+        The update function takes the ip address and hostname passed into the function and adds it to the host file.
+        :param ipaddress:
+        :param hostname:
+        """
+        if 'linux' in sys.platform:
+            filename = '/etc/hosts'
+        else:
+            log.log(log.LOG_ERROR, "your system platform is not linux base")
+            sys.exit(1)
+
+        outputfile = open(filename, 'a')
+        entry = "\n" + ipaddress + "\t" + hostname + "\n"
+        outputfile.writelines(entry)
+        outputfile.close()
+
+    def validip(ipaddress):
+        """ str -> bool
+        The function takes the IP address as a string and splits it by ".". It then checks to see if there are 4 items
+        in the list. If not, it's not valid. Next, it makes sure the last two characters are not ".0", which would signify an
+        invalid address. Third it checks the last character to make sure it's not a ".", which would be invalid. Lastly, it
+        checks each item to make sure it's greater than 0 or equal to zero but less than or equal to 255.
+        :param ipaddress:
+        :return:
+        """
+        parts = ipaddress.split(".")
+        if len(parts) != 4:
+            return False
+        if ipaddress[-2:] == '.0': return False
+        if ipaddress[-1] == '.': return False
+        for item in parts:
+            if not 0 <= int(item) <= 255:
+                return False
+        return True
+
+    def isValidHostname(hostname):
+        """ str -> bool
+        First it checks to see if the hostname is too long. Next, it checks to see if the first character is a number.
+        If the last character is a ".", it is removed. A list of acceptable characters is then compiled and each section
+        of the host name, split by any ".", is checked for valid characters. If there everything is valid, True is returned.
+        :param hostname:
+        :return:
+        """
+        if len(hostname) > 255:
+            return False
+        if hostname[0].isdigit(): return False
+        if hostname[-1:] == ".":
+            hostname = hostname[:-1] # strip exactly one dot from the right, if present
+        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        return all(allowed.match(x) for x in hostname.split("."))
+
+        def get_host(self, host_id):
+            host = self.fm.hosts.show(id=host_id)
+            return host
+
+        def remove_host(self, host_id):
+            try:
+                self.fm.hosts.destroy(id=host_id)
+                return True
+            except:
+                return False
