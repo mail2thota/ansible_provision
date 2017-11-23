b78f1a84a4885bc1ef6fd0162a5ce1af2f11d6ab
mradhakrishnan <manikandan.radhakrishnan@baesystems.com>
2017-11-23 17:38:24 +0800

    changes for etc/hosts to retain the entry which exists already

--- ansible/mdr_cluster/roles/init/tasks/all_config/localhost_config.sh
+++ ansible/mdr_cluster/roles/init/tasks/all_config/localhost_config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-host_list_path=$1
+hosts_file=$(awk '$1=$1' ORS='\\n' /etc/hosts)
+if [[ $hosts_file == *"#cluster nodes start"* ]];then
+sed -i '/start/,/end/d' /etc/hosts
+fi
+echo "#cluster nodes start" >>/etc/hosts
 cat /home/all_config/host_list  >> /etc/hosts
-
+echo "#cluster nodes end" >> /etc/hosts
