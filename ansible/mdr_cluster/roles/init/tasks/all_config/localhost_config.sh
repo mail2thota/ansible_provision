bf6aee709e4f1556a9f03fc3304a8ef597ba4754
mradhakrishnan <manikandan.radhakrishnan@baesystems.com>
2017-11-24 09:55:40 +0800

    Improvements-entry into etc/hosts only once when everytime playbook is run

--- ansible/mdr_cluster/roles/init/tasks/all_config/localhost_config.sh
+++ ansible/mdr_cluster/roles/init/tasks/all_config/localhost_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 hosts_file=$(awk '$1=$1' ORS='\\n' /etc/hosts)
-if [[ $hosts_file == *"#cluster nodes start"* ]];then
-sed -i '/start/,/end/d' /etc/hosts
+if [[ $hosts_file == *"#mdr_hosts_begins"* ]];then
+sed -i '/mdr_hosts_begins/,/mdr_hosts_ends/d' /etc/hosts
 fi
-echo "#cluster nodes start" >>/etc/hosts
+echo "#mdr_hosts_begins" >>/etc/hosts
 cat /home/all_config/host_list  >> /etc/hosts
-echo "#cluster nodes end" >> /etc/hosts
+echo "#mdr_hosts_ends" >> /etc/hosts
