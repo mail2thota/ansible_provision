907ccc0b92eb1d86b8bab80cb2524a2c12cbe56a
Sateesh Telaprolu <STelaprolu@Imp.net>
2017-12-07 03:06:33 +0000

    Removed Previous forman directory

--- foreman-ansible/ansible/roles/fmclient/files/foreman_yml/src/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf8 -*-
-#Copyright:@BaeSystemsAI
-#Original author: Heri Sutrisno
-#Email:harry.sutrisno@baesystems.com
-#Description:init file for python 
-
-import os
-
--- foreman-ansible/ansible/roles/fmclient/files/foreman_yml/src/version.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#Copyright:@BaeSystemsAI
-#Original author: Heri Sutrisno
-#Email:harry.sutrisno@baesystems.com
-#Description:product version
-
-"""Version module to be read from various places"""
-__version__ = "0.0.1"
