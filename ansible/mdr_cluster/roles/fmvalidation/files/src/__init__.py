62645cdaa1a4c28813e94b7ae6aa10286e08f26c
Sateesh Telaprolu <STelaprolu@Imp.net>
2017-12-07 07:05:58 +0000

    Updating validation

---
+++ ansible/mdr_cluster/roles/fmvalidation/files/src/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#Copyright:@BaeSystemsAI
+#Original author: Heri Sutrisno
+#Email:harry.sutrisno@baesystems.com
+#Description:init file for python 
+
+import os
+
