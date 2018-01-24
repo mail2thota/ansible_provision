a605cc1e1cf564297ae8c2efe1e79878b9e465b7
Sateesh Telaprolu <STelaprolu@Imp.net>
2018-01-24 10:22:49 +0000

    Copyin etc host file scripts mapped to inventoryname

--- ansible/mdr_cluster/roles/init/scripts/localhost_config.sh
+++ ansible/mdr_cluster/roles/init/scripts/localhost_config.sh
@@ -4,5 +4,5 @@ if [[ $hosts_file == *"#mdr_hosts_begins"* ]];then
 sed -i '/mdr_hosts_begins/,/mdr_hosts_ends/d' /etc/hosts
 fi
 echo "#mdr_hosts_begins" >>/etc/hosts
-cat /home/all_config/host_list  >> /etc/hosts
+cat /home/$1/host_list  >> /etc/hosts
 echo "#mdr_hosts_ends" >> /etc/hosts
