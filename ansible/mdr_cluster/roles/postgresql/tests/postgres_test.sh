532f0b36e1f2b3dbb8d032f33884d3df4e0d9183
hsutrisno <harry.sutrisno@baesystems.com>
2018-02-13 08:54:14 +0000

    fixed postgresql test and prevent proxy's block

--- ansible/mdr_cluster/roles/postgresql/tests/postgres_test.sh
+++ ansible/mdr_cluster/roles/postgresql/tests/postgres_test.sh
@@ -5,4 +5,4 @@ if psql -U postgres -lqt | cut -d \| -f 1 | grep -qw postgres; then
 else
     echo "Postgres Test Failed"
 fi
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+sudo psql -U postgres -c "ALTER USER postgres PASSWORD 'postgres';"
