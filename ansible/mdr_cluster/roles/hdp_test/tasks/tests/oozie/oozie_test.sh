ed7abf44341498f0f890a7bd1fd7d5c95b7bea24
hsutrisno <harry.sutrisno@baesystems.com>
2018-02-15 04:04:42 +0000

    fixed tests cases and postgresql test

--- ansible/mdr_cluster/roles/hdp_test/tasks/tests/oozie/oozie_test.sh
+++ ansible/mdr_cluster/roles/hdp_test/tasks/tests/oozie/oozie_test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
 oozie_url=$1 
 oozie_name_node=$2 
 oozie_job_tracker=$3
 echo "${oozie_url}"
 sed -i "s%<OOZIE_NAME_NODE>%${oozie_name_node}%g" /home/hdfs/oozie/oozie-examples/job.properties
 sed -i "s%<OOZIE_JOB_TRACKER>%${oozie_job_tracker}%g" /home/hdfs/oozie/oozie-examples/job.properties 
+hdfs dfs -test -d /workflows
+if [ $? == 0 ]; then
+    hdfs dfs -rm -r -f /workflows
+fi
 
-hadoop fs -rm -r /workflows/
 hadoop fs -mkdir /workflows/
 hadoop fs -put /home/hdfs/oozie/oozie-examples /workflows/oozie-examples
 export OOZIE_URL="${oozie_url}"
 oozie job -config /home/hdfs/oozie/oozie-examples/job.properties -run
+
